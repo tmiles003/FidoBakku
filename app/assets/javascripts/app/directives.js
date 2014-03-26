1baa1d8c846300bd049747c94e42aafe12ae455f
tom <tom@binlid.net>
2014-03-26 15:24:36 +0800

    Highlight nav position

--- app/assets/javascripts/app/directives.js
+++ app/assets/javascripts/app/directives.js
@@ -1,3 +1,22 @@
 'use strict';
 
 /* Directives */
+
+fiApp.directive('activeNavLink', ['$location', function(location) {
+  return {
+    restrict: 'A',
+    link: function(scope, element, attrs, controller) {
+      var clas_ = attrs.activeNavLink;
+      var path = attrs.href;
+      path = path.substring(2); // fix this
+      scope.location = location;
+      scope.$watch('location.path()', function(newPath) {
+        if (path === newPath) {
+          element.addClass(clas_);
+        } else {
+          element.removeClass(clas_);
+        }
+      });
+    }
+  };
+}]);
