119754b8d4cd74d29b2c549dcfe51304f64b0347
tom <tom@binlid.net>
2014-04-01 16:21:50 +0800

    Goal flags: strings not needed after all

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,15 +1,6 @@
 class GoalSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :due_date, :is_private, :is_complete, :goal_path
   
-  # strings seem to be better...
-  def is_private
-    object.is_private ? '1' : '0'
-  end
-  
-  def is_complete
-    object.is_complete ? '1' : '0'
-  end
-  
   def goal_path
     root_path(anchor: goal_manage_path(object))
   end
