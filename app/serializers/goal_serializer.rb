01a3a7e0b1895b36b4f2eff7ae7ae22f34e09c36
tom <tom@binlid.net>
2014-04-01 16:41:59 +0800

    Due date format in goal form

--- app/serializers/goal_serializer.rb
+++ app/serializers/goal_serializer.rb
@@ -1,6 +1,10 @@
 class GoalSerializer < ActiveModel::Serializer
   attributes :id, :title, :content, :due_date, :is_private, :is_complete, :goal_path
   
+  def due_date
+    object.due_date = object.due_date.strftime('%d/%m/%Y') unless object.due_date.nil?
+  end
+  
   def goal_path
     root_path(anchor: goal_manage_path(object))
   end
