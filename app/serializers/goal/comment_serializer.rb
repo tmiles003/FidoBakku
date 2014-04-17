8cf3b9689ed4171d96ae1d44ec85886064343aaa
tom <tom@binlid.net>
2014-04-17 13:44:13 +0800

    Goal page update

--- app/serializers/goal/comment_serializer.rb
+++ app/serializers/goal/comment_serializer.rb
@@ -4,6 +4,10 @@ class Goal::CommentSerializer < ActiveModel::Serializer
   has_one :user, serializer: ::Goal::UserSerializer
   
   def created_at
+    object.created_at.strftime('%e %b %y, %k:%M') unless object.created_at.nil?
+  end
+  
+  def created_at_ts
     object.created_at.strftime('%s') unless object.created_at.nil?
   end
   
