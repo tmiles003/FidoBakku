9f735f023d835f04995465da0669d171876b1493
tom <tom@binlid.net>
2014-04-19 18:34:47 +0800

    Fix account controller

--- app/controllers/api/admin/account_controller.rb
+++ app/controllers/api/admin/account_controller.rb
@@ -2,16 +2,18 @@ class Api::Admin::AccountController < Api::Admin::ApiController
   
   authorize_resource
   
-  prepend_before_filter 
+  prepend_before_filter :set_account
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_account
   
   # GET /api/account.json
   def show
+    render json: @account
   end
   
   # PATCH/PUT /api/account.json
   def update
     if @account.update(account_params)
-      head :no_content
+      render json: @account
     else
       render json: @account.errors
     end
@@ -21,6 +23,17 @@ class Api::Admin::AccountController < Api::Admin::ApiController
   # laterrr. cos special case
   
   private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_account
+      @account = current_user.account
+    end
+    
+    def invalid_account
+      logger.error 'No account with this id'
+      ## redirect to dashboard
+      head :no_content
+    end
+    
     # Never trust parameters from the scary internet, only allow the white list through.
     def account_params
       params.required(:account).permit(:name, :owner_id)
