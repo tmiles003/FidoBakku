d7b632e20f8eb908578710cb3f93d1a6d229d4bb
tom <tom@binlid.net>
2014-04-21 18:56:21 +0800

    Comment out missing serializer

--- app/serializers/dashboard/user_evaluation_serializer.rb
+++ app/serializers/dashboard/user_evaluation_serializer.rb
@@ -1,6 +1,6 @@
 class Dashboard::UserEvaluationSerializer < ActiveModel::Serializer
   attributes :id, :progress, :evaluation_path
-  has_one :evaluation, serializer: ::Admin::EvaluationSerializer
+  #has_one :evaluation, serializer: ::Admin::EvaluationSerializer
   
   def evaluation_path
     root_path(anchor: user_evaluation_path(object))
