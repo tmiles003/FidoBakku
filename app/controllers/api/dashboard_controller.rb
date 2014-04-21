8099e1d45b3976366b5a42ce4990aacaca9a73ef
tom <tom@binlid.net>
2014-04-21 18:42:20 +0800

    Dashboard: comment out queries due to failing serializers

--- app/controllers/api/dashboard_controller.rb
+++ app/controllers/api/dashboard_controller.rb
@@ -1,16 +1,19 @@
 class Api::DashboardController < Api::ApiController
   
   def evaluations
-    render json: ::UserEvaluation.where(evaluator_id: current_user.id).includes(evaluation: :user), 
-      each_serializer: Dashboard::UserEvaluationSerializer
+    render nothing: true
+    #render json: ::UserEvaluation.where(evaluator_id: current_user.id).includes(evaluation: :user), 
+    #  each_serializer: Dashboard::UserEvaluationSerializer
   end
   
   def feedbacks
-    render json: ::Evaluation.all, each_serializer: Dashboard::EvaluationSerializer
+    render nothing: true
+    #render json: ::Evaluation.all, each_serializer: Dashboard::EvaluationSerializer
   end
   
   def users
-    render json: current_user.account.users, each_serializer: Dashboard::UserSerializer
+    render nothing: true
+    #render json: current_user.account.users, each_serializer: Dashboard::UserSerializer
   end
   
 end
