3a722a4ed08ec8e7dc2a4201d305179c3442eaaa
tom <tom@binlid.net>
2014-04-22 12:57:55 +0800

    Profile password update

--- app/assets/javascripts/app/profile/controllers.js
+++ app/assets/javascripts/app/profile/controllers.js
@@ -5,11 +5,23 @@
 fiApp.controller('ProfileCtrl', ['$scope', '$http', 'NotifSrv', 
                  function($scope, $http, NotifSrv) {
   
-  $scope.saveForm = function(passwordForm) {
-    $http.put('/api/profile.json', passwordForm)
-      .success(function() {
-        NotifSrv.success();
-      });
+  $scope.updatePassword = function(passwordForm, isValid) {
+    $scope.submitted = true;
+    if (isValid) {
+      $scope.submitted = false;
+      $http.put('/api/profile', passwordForm).success(function() {
+          $scope.submitted = false;
+          $scope.password = {};
+          $scope.passwordForm.$setPristine();
+          NotifSrv.success();
+        });
+    }
+  }
+  
+  $scope.clearForm = function() {
+    $scope.submitted = false;
+    $scope.password = {};
+    $scope.passwordForm.$setPristine();
   }
   
 }]);
--- app/controllers/api/profile_controller.rb
+++ app/controllers/api/profile_controller.rb
@@ -1,10 +1,12 @@
 class Api::ProfileController < Api::ApiController
   
+  prepend_before_filter :set_user, only: [:update]
+  rescue_from ActiveRecord::RecordNotFound, with: :invalid_user
+  
   def update
-    @user = ::User.find(current_user.id)
     if @user.update(passwd_params)
       sign_in @user, :bypass => true
-      head :no_content
+      head :no_content, status: :ok
     else
       render json: @user.errors, status: :unprocessable_entity
     end
@@ -15,5 +17,15 @@ class Api::ProfileController < Api::ApiController
     def passwd_params
       params.required(:profile).permit(:password, :password_confirmation)
     end
+    
+    def set_user
+      @user = ::User.find(current_user.id) unless current_user.nil?
+    end
   
+    def invalid_user
+      logger.error "No user with this id: nil"
+      error = Hash['error', [t('admin.users.record_not_found')]]
+      render json: error, status: :not_found
+    end
+
 end
