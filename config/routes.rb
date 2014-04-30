c8f0fd6fc385d6fef4d8700925eedb932366159b
tom <tom@binlid.net>
2014-04-30 10:57:25 +0800

    Sign in/out wording

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,7 @@ FidoBakku::Application.routes.draw do
 
   devise_for :users, 
     :path => '',
-    :path_names => { :sign_in => 'login', :sign_out => 'logout' },
+    :path_names => { :sign_in => 'signin', :sign_out => 'signout' },
     :skip => [:registrations]
   
   # get '/' => 'welcome#application', as: :dashboard
