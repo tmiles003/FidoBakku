26aac45bd2bbb5eb967f0959ad5023118ab52dd9
tom <tom@binlid.net>
2014-05-04 17:44:07 +0530

    Minor update to account serializer

--- app/serializers/admin/account_serializer.rb
+++ app/serializers/admin/account_serializer.rb
@@ -2,6 +2,16 @@ class Admin::AccountSerializer < ActiveModel::Serializer
   
   attributes :id, :name, :owner_id, :admins, :plan, :expires_at
   
+  def plan
+    plan = object.plan
+    
+    if 'premium' == object.plan && !object.expires_at.nil?
+      plan = 'demo'
+    end
+    
+    plan.capitalize
+  end
+  
   def admins
     admin_users = current_user.account.users.where(role: 'admin')
     ActiveModel::ArraySerializer.new(admin_users, each_serializer: ::Admin::AdminUserSerializer)
